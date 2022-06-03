676c92d27b8d5a89e4ceb343334295eaed41b803
sama <samaalfares@yahoo.com>
2022-06-03 19:13:03 +0300

    commit on 'main'

---
+++ public/js/Profile.js
@@ -0,0 +1,6 @@
+const moreBtn = document.querySelector(".btn-more");
+const more = document.querySAelector(".more");
+
+moreBtn.addEventListener("click", () => {
+  more.classList.toggle("active");
+});
